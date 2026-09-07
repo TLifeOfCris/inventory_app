1e0da119639494cfd8f484bc364fca948814dd29
TLifeOfCris <crissandoval150201@gmail.com>
2026-09-07 10:53:07 -0600

    Se agregó el model de la caja

---
+++ lib/models/box.dart
@@ -0,0 +1,16 @@
+class Box {
+  final String id;
+  final String name;
+  final String location;
+  final String description;
+  final List items;
+
+
+  Box ({
+    required this.id,
+    required this.name,
+    required this.location,
+    required this.description,
+    required this.items
+  });
+}
