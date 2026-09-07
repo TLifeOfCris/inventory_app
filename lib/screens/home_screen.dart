33bfd83f2a5863618697de7703f35546799cdfee
TLifeOfCris <crissandoval150201@gmail.com>
2026-09-07 11:59:05 -0600

    Se agregó boton para escanear cajas

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,38 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Mi Inventario'),
       ),
-      body: Container(),
+      body: Container(
+        
+        child:
+        Column(
+          children: <Widget>[
+            
+            //Escanear Caja 
+            Card(
+              child: InkWell(
+                onTap: () {
+                // aqui después abrir el escaner
+                },
+                borderRadius: BorderRadius.circular(12),
+                child: Padding(padding: EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    Icon(Icons.qr_code_scanner),
+                    SizedBox(width: 16,),
+                    Expanded(child: Text('Escanear caja', textAlign: TextAlign.center,))
+                  ],
+                ),),
+              ),
+            ),
+
+
+            //para la lista de cajas
+            Expanded(child: ListView(
+              
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
