50fdfa21126a772a5391039f3315636cc4730dc0
TLifeOfCris <crissandoval150201@gmail.com>
2026-09-14 15:04:04 -0600

    Se agregó Provider a app.dart

--- lib/providers/boxes_provider.dart
+++ lib/providers/boxes_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/box.dart';
 class BoxesProvider extends ChangeNotifier {
+  //aquí se guardarán las cajas que se crean, nombre, descripci+on, etc
   final List<Box> _boxes = [];
 }
